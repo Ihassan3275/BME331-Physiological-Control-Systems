b79cf37d516adadca636b06b55b445d8f14bd8c3
Ihassan3275 <86846180+Ihassan3275@users.noreply.github.com>
2024-03-21 19:30:55 -0400

    Add files via upload

---
+++ BME331-Labs/Lab-3/Quiet_EC_Experiment_/lab3_export_func.m
@@ -0,0 +1,16 @@
+
+% Read the data from the 'quiet_standing.data' file as text
+data = readmatrix('quiet_standing_ec.data', 'FileType', 'text');
+
+% Get the number of columns in the data matrix
+numCols = size(data, 2);
+
+% Loop through each column and write it to a .txt file
+for colIdx = 1:numCols
+    colData = data(:, colIdx);
+    fileName = sprintf('column%d.txt', colIdx);
+    writematrix(colData, fileName);
+end
+
+% col 1 and col 3 are the same -> AP
+% col 2 -> TA
